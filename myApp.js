e46c4f0a7c3b4305a6a00dfa77b7d8f93693933b
Artemis <minhquan.nguyen.fr@gmail.com>
2020-03-11 12:22:54 +0100

    Exo 8

--- myApp.js
+++ myApp.js
@@ -52,7 +52,12 @@ app.get("/json", function(req, res){
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', function(req, res, next) {
+    req.time = new  Date().toString();  // Hypothetical synchronous operation
+    next();
+  }, function(req, res) {
+    res.send(req.time);
+  });
 
 /** 9)  Get input from client - Route parameters */
 
